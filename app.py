2777c1ee7756be843424c625a2a59eca23b54545
Suey Chen <xchen3@wayfair.com>
2022-06-25 20:15:20 -0400

    sending and acknowledging notification

--- app.py
+++ app.py
@@ -2,8 +2,105 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+import logging
+# Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+logger = logging.getLogger(__name__)
+
+payload = {
+	"blocks": [
+		{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": "Where are you working from today?"
+			}
+		},
+		{
+			"type": "section",
+			"fields": [
+				{
+					"type": "mrkdwn",
+					"text": "*When:*\nJun 25"
+				}
+			]
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "Office"
+					},
+					"style": "primary",
+					"value": "click_me_123",
+                    "action_id": "check_button"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "Home"
+					},
+					"style": "primary",
+					"value": "click_me_123",
+                    "action_id": "check_button_2"
+				}
+			]
+		}
+	]
+}
+
+channel_name = "getting-people-connected"
+conversation_id = None
+try:
+    # Call the conversations.list method using the WebClient
+    for result in client.conversations_list():
+        if conversation_id is not None:
+            break
+        for channel in result["channels"]:
+            if channel["name"] == channel_name:
+                conversation_id = channel["id"]
+                #Print result
+                print(f"Found conversation ID: {conversation_id}")
+                break
+
+except SlackApiError as e:
+    print(f"Error: {e}")
+
+
+# ID of the channel you want to send the message to
+channel_id = conversation_id
+
+try:
+    # Call the chat.postMessage method using the WebClient
+    result = client.chat_postMessage(
+        channel=channel_id,
+        text="notification to user",
+        blocks=payload["blocks"]
+    )
+    logger.info(result)
+
+except SlackApiError as e:
+    logger.error(f"Error posting message: {e}")
+
+
+# This listener will be called every time an interactive component with the `action_id` "approve_button" is triggered
+# `block_id` is disregarded in this case
+@app.action("check_button")
+def some_action_response(ack,say):
+    ack()
+    # Do something in response
+    say("Thank you! I have updated your working location!")
+
 
 # Listens to incoming messages that contain "hello"
 @app.message("hello")
